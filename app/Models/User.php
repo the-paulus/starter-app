f06856c5bded1ec1266175edb0b6030475e1ce4b
Paul Lyon <pmlyon@gmail.com>
2018-04-13 16:33:03 -0500

    UserGroup controller, model, migration, factory, seeder, and test and added group to User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -103,4 +103,9 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function groups() {
+
+        $this->hasMany(UserGroup::class);
+    }
 }
---
+++ database/factories/UserGroupFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'name' => substr($faker->unique()->word, 0, 12),
+        'description' => $faker->sentence(12),
+    ];
+});
