c4141d286467612a86d4c740b69b8e4833130bfa
Paul Lyon <pmlyon@gmail.com>
2018-04-13 03:06:18 -0500

    Added new database migration for user table.

---
+++ database/migrations/2018_04_13_054241_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('first_name')->after('id');
+            $table->string('last_name')->after('first_name');
+            $table->string('auth_type')->after('password');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('auth_type');
+            $table->dropSoftDeletes();
+            $table->string('name')->after('id');
+        });
+    }
+}
