678738d4ab702098f12ae1c21aeb19ff83d88b3d
Paul Lyon <pmlyon@gmail.com>
2018-04-16 22:09:03 -0500

    Updated factories to use proper namespace for classes.

--- database/factories/UserGroupFactory.php
+++ database/factories/UserGroupFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Models\UserGroup::class, function (Faker $faker) {
     return [
         'name' => substr($faker->unique()->word, 0, 12),
         'description' => $faker->sentence(12),
