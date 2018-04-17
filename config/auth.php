be9f03e2ecf5bb2ac939b97bf6fa188ec5702dda
Paul Lyon <pmlyon@gmail.com>
2018-04-16 23:32:03 -0500

    Updated config/auth.php doc to reflect User controller class name change.

--- config/auth.php
+++ config/auth.php
@@ -49,7 +49,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | User Providers
+    | UserController Providers
     |--------------------------------------------------------------------------
     |
     | All authentication drivers have a user provider. This defines how the
