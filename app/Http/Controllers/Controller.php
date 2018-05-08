fbd526cec87eede9fb18f3c0a71f6712c4e41896
Paul Lyon <pmlyon@gmail.com>
2018-05-07 23:38:33 -0500

    Added Route generation to Controller class.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,11 +7,15 @@ use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Route;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * Standard controller HTTP response codes that are used when associated operations are successful.
+     */
     const METHOD_SUCCESS_CODE = [
         'index'     => Response::HTTP_OK,
         'show'      => Response::HTTP_OK,
@@ -20,6 +24,9 @@ class Controller extends BaseController
         'destroy'   => Response::HTTP_GONE
     ];
 
+    /**
+     * Standard controller HTTP response codes that are used when associated operations have failed.
+     */
     const METHOD_FAILURE_CODE = [
         'index'     => Response::HTTP_NO_CONTENT,
         'show'      => Response::HTTP_NOT_FOUND,
@@ -27,4 +34,102 @@ class Controller extends BaseController
         'update'    => Response::HTTP_NOT_MODIFIED,
         'destroy'   => Response::HTTP_I_AM_A_TEAPOT
     ];
+
+    /**
+     * @var bool Indicates whether or not the controller should create the standard API routes.
+     */
+    protected static $createApiRoutes = TRUE;
+
+    /**
+     * @var bool Indicates whether or not the controller should create the standard web routes.
+     */
+    protected static $createWebRoutes = TRUE;
+
+    /**
+     * Calls Route::apiResource() to create the standard API endpoints for the controller.
+     *
+     * When creating the routes, the base class name of the model attached to the controller will be used.
+     *
+     * Example, If the controller has the $model set to User::class, and the Controller's class name is UserController,
+     * then this function is identical to calling:
+     *
+     * Route::apiResource('user', 'User');
+     */
+    private function standardAPIRoutes() {
+
+        Route::apiResource(strtolower(self::$model::baseModelClassName()), __CLASS__);
+
+    }
+
+    /**
+     * This is intended to be overridden in the inherited class to allow the developer to add specialized routes.
+     *
+     * If the application requires any changes to the standard routes created with
+     * Route::apiResource('path', 'Callback'), then the $createApiRoutes should be set to false and all routes manually
+     * created in the overridden method.
+     */
+    public static function customAPIRoutes() {
+
+        // Add code here.
+
+    }
+
+    /**
+     * Calls Route::resource() to create the standard URLs for the controller.
+     *
+     * When creating the routes, the base class name of the model attached to the controller will be used.
+     *
+     * Example, If the controller has the $model set to User::class, and the Controller's class name is UserController,
+     * then this function is identical to calling:
+     *
+     * Route::resource('user', 'User');
+     */
+    private function standardWebRoutes() {
+
+        Route::resource(strtolower(self::$model::baseModelClassName()), __CLASS__);
+
+    }
+
+    /**
+     * This is intended to be overridden in the inherited class to allow the developer to add specialized routes.
+     *
+     * If the application requires any changes to the standard routes created with
+     * Route::resource('path', 'Callback'), then the $createWebRoutes should be set to false and all routes manually
+     * created in the overridden method.
+     */
+    public static function customWebRoutes() {
+
+        // Add code here.
+
+    }
+
+    /**
+     * Calls any methods to create api routes for the controller.
+     */
+    public static function createApiRoutes() {
+
+        if( self::$createApiRoutes ) {
+
+            self::standardAPIRoutes();
+
+        }
+
+        static::customAPIRoutes();
+
+    }
+
+    /**
+     * Calls any methods to create web routes for the controller.
+     */
+    public static function createWebRoutes() {
+
+        if( self::$createWebRoutes ) {
+
+            self::standardWebRoutes();
+
+        }
+
+        static::customWebRoutes();
+
+    }
 }
