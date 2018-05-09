ce9c11e41d1a05b7be0930b71fc7bca38880f5af
Paul Lyon <pmlyon@gmail.com>
2018-05-08 23:36:52 -0500

    Updated DatabaseSeeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,47 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+    private $model_tables = [
+        User::class,
+        UserGroup::class,
+        Permission::class,
+        Setting::class,
+        SettingGroup::class,
+    ];
+
+    private $pivot_tables = [
+        'user_user_groups',
+        'permission_user_groups',
+    ];
+
+    private $seeders = [
+        UserGroupsTableSeeder::class,
+        UsersTableSeeder::class,
+    ];
+
+    public function startClean() {
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        foreach($this->pivot_tables as $pt) {
+
+            DB::table($pt)->truncate();
+
+        }
+
+        foreach($this->model_tables as $mt) {
+
+            $mt::truncate();
+
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
     /**
      * Run the database seeds.
      *
@@ -11,6 +49,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        foreach($this->seeders as $seeder) {
+
+            $this->call($seeder);
+
+        }
     }
 }
