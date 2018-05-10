ac7ce14e0882dcff334f0b0df64fe4fe1a9cb48d
Paul Lyon <pmlyon@gmail.com>
2018-05-10 00:04:23 -0500

    Updated UsersTableSeeder
    - Added use statements.
    - Added default encrypted password when saving.
    - Fixed UserGroup attachments to users.
    - Formatted error message.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,32 +2,33 @@
 
 use App\Models\User;
 use App\Models\UserGroup;
+use Illuminate\Support\Arr;
 
-use \Illuminate\Validation\ValidationException;
+use Illuminate\Validation\ValidationException;
 
 class UsersTableSeeder extends DatabaseSeeder
 {
 
-    private $users = [
+    public static $users = [
         [
             'first_name' => 'Application',
             'last_name' => 'Admin',
             'email' => 'admin@starter-app.local',
-            'password' => '',
+            'password' => 'adminpasssecret',
             'auth_type' => 'local',
         ],
         [
             'first_name' => 'Admin',
             'last_name' => 'User',
             'email' => 'admin-user@starter-app.local',
-            'password' => '',
+            'password' => 'adminsecretagentuser',
             'auth_type' => 'local',
         ],
         [
             'first_name' => 'Standard',
             'last_name' => 'User',
             'email' => 'user@starter-app.local',
-            'password' => '',
+            'password' => 'justastandardpassword',
             'auth_type' => 'local',
         ]
     ];
@@ -40,19 +41,22 @@ class UsersTableSeeder extends DatabaseSeeder
     public function run()
     {
 
-        foreach($this->users as $key => $value) {
+        foreach(self::$users as $key => $value) {
 
             try {
 
-                $user = User::validateAndCreate($value);
+                $value['password'] = bcrypt($value['password']);
 
-                $user->groups()->attache(UserGroup::all(['id'])->where('name', '=', UserGroupsTableSeeder::$groups[$key])->id);
+                $user = User::validateAndCreate($value);
+                $user_group = UserGroup::all()->where('name', '=', UserGroupsTableSeeder::$groups[$key]['name'])->first();
+
+                $user->groups()->attach($user_group);
 
                 $this->command->info('Created ' . $value['first_name'] . ' ' . $value['last_name'] . '; added to ' . UserGroupsTableSeeder::$groups[$key]['name'] . ' group.');
 
             } catch(ValidationException $validationException) {
 
-                $this->command->info($validationException->getMessage());
+                $this->command->info(print_r(implode("\n",Arr::flatten($validationException->errors())), TRUE));
 
             }
         }
