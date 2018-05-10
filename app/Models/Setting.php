11fa0afe0fd4c09b6aeb371fb7408b5b6a3a47aa
Paul Lyon <pmlyon@gmail.com>
2018-05-10 15:06:13 -0500

    Setting inherits from BaseModel now.

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Setting extends Model
+class Setting extends BaseModel
 {
     /**
      * The name of the create column name in the database.
