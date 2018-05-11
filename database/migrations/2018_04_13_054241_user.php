9ce2f1691cdde760918a9a7b1a7386b6874ebdc6
Paul Lyon <pmlyon@gmail.com>
2018-05-11 17:57:00 -0500

    Updated user table migration.
    - Created auth_types table.
    - Changed auth_type from string to integer column.
    - Added foreign key constraint for auth_type.
    - Added DropTableIfExists for auth_types.

--- database/migrations/2018_04_13_054241_user.php
+++ database/migrations/2018_04_13_054241_user.php
@@ -13,15 +13,27 @@ class User extends Migration
      */
     public function up()
     {
+        Schema::create('auth_types', function(Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->softDeletes();
+
+        });
+
         Schema::table('users', function(Blueprint $table) {
+
             $table->dropColumn('name');
             $table->string('first_name')->nullable(true)->after('id');
             $table->string('last_name')->nullable(true)->after('first_name');
-            $table->string('auth_type')->nullable(true)->default('local')->after('password');
+            $table->unsignedInteger('auth_type')->after('password');
             $table->softDeletes();
 
             $table->index('first_name');
             $table->index('last_name');
+
+            $table->foreign('auth_type', 'auth_type_fk')->references('id')->on('auth_types');
+
         });
     }
 
@@ -32,6 +44,8 @@ class User extends Migration
      */
     public function down()
     {
+
+        Schema::dropIfExists('auth_types');
 
     }
 }
