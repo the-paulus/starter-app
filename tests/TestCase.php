1be61566128c0825b9cd5abc33ee1b710f26cf2c
Paul Lyon <pmlyon@gmail.com>
2018-05-13 18:33:46 -0500

    Added invokePrivateMethod method.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,11 @@ abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase, CreatesApplication;
 
+    public function invokePrivateMethod(&$object, string $method, array $parameters = array()) {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(TRUE);
+        return $method->invokeArgs($object, $parameters);
+    }
+
 }
