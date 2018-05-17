0600eb3c81581b52ee3b8a004a445ea5eb2bf526
Paul Lyon <pmlyon@gmail.com>
2018-05-17 14:38:47 -0500

    Comments and updated store and update methods to dynamically call model mutators.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,54 +5,87 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use App\Models\UserGroup;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 
+/**
+ * Class UserController handles CRUD requests.
+ *
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
 
+    /**
+     * @var string $model Model class that the controller is bound to.
+     */
     protected static $model = User::class;
 
+    /**
+     *
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index() {
 
         return response()->json(['data' => User::all()], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
 
     }
 
+    /**
+     *
+     *
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($id) {
 
-        $user = null;
+        $user = User::findOrFail($id);
 
-        try {
-
-            $user = User::findOrFail($id);
-
-            return response()->json(['data' => [$user]], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
-
-        } catch(ModelNotFoundException $e) {
-
-            return response()->json(['data' => [], 'errors' => 'User ID ' . $id . ' was not found.'], self::METHOD_FAILURE_CODE[__FUNCTION__]);
-
-        }
+        return response()->json(['data' => [$user]], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
 
     }
 
-    public function store(Request $request) {
+    /**
+     * Callback function used in array_walk to call model mutators.
+     *
+     * @param $item     Array   Element being analyzed.
+     * @param $key      Array   Element's key.
+     * @param $model    Model   Model to perform mutations on.
+     */
+    private function callMutator(&$item, $key, $model) {
 
-        $user = null;
+        $mutations = $model->getMutatedAttributes();
+
+        if( in_array($key, $mutations) ) {
+
+            $func = ucfirst('set'.camel_case($key).'Attribute');
+
+            $model->$func($item);
+
+        }
+    }
+
+    /**
+     *
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) {
 
         try {
 
             // TODO: Ensure that relationship rules are validated.
             $user = User::validateAndCreate($request->all());
+            $data = $request->all();
 
-            if( $request->has('user_group_ids') ) {
-
-                $user->groups()->sync($request->get('user_group_ids'));
-
-            }
+            array_walk($data, array(self::class, 'callMutator'), $user);
 
             return response()->json(['data' => [$user->freshRelationships()]], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
 
@@ -64,25 +97,24 @@ class UserController extends Controller
 
     }
 
+    /**
+     *
+     *
+     * @param Request $request
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, $id) {
-
-        $user = null;
 
         try {
 
             $user = User::find($id)->validateAndUpdate($request->all());
+            $data = $request->all();
 
-            if ($request->has('user_group_ids')) {
-
-                $user->groups()->sync($request->get('user_group_ids'));
-
-            }
+            array_walk($data, array(self::class, 'callMutator'), $user);
 
             return response()->json(['data' => [$user->freshRelationships()]], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
-
-        } catch(ModelNotFoundException $modelNotFoundException) {
-
-            return response()->json(['data' => [], 'errors' => [User::baseModelClassName() . ' not found.']], Response::HTTP_NOT_FOUND);
 
         } catch(ValidationException $validationException) {
 
@@ -92,21 +124,18 @@ class UserController extends Controller
 
     }
 
+    /**
+     *
+     *
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy($id) {
 
-        $user = null;
+        User::findOrFail($id)->delete();
 
-        try {
-
-            User::findOrFail($id)->delete();
-
-            return response()->json(['data' => []], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
-
-        } catch( ModelNotFoundException $modelNotFoundException) {
-
-            return response()->json(['data' => [], 'errors' => [User::baseModelClassName() . ' not found.']], Response::HTTP_NOT_FOUND);
-
-        }
+        return response()->json(['data' => []], self::METHOD_SUCCESS_CODE[__FUNCTION__]);
 
     }
 }
