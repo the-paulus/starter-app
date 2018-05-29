afab84cf5c5cc6edd1fa290a837e7bf3a80377a4
Paul Lyon <pmlyon@gmail.com>
2018-05-29 18:47:00 -0500

    Updated UserGroupPolicy.

--- app/Policies/UserGroupPolicy.php
+++ app/Policies/UserGroupPolicy.php
@@ -2,71 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\UserGroup;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
-class UserGroupPolicy
+class UserGroupPolicy extends ApplicationPolicy
 {
-    use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the user group.
-     *
-     * @param  User        $user
-     * @param  UserGroup   $userGroup
-     *
-     * @return bool
-     */
-    public function view(User $user, UserGroup $userGroup)
-    {
+    protected static $model = UserGroup::class;
 
-        return $user->hasPermission(UserGroup::getModelPermission('access'));
-
-    }
-
-    /**
-     * Determine whether the user can create user groups.
-     *
-     * @param  User         $user
-     * @return bool
-     */
-    public function create(User $user)
-    {
-
-        return $user->hasPermission(UserGroup::getModelPermission('create'));
-
-    }
-
-    /**
-     * Determine whether the user can update the user group.
-     *
-     * @param  User         $user
-     * @param  UserGroup    $userGroup
-     *
-     * @return bool
-     */
-    public function update(User $user, UserGroup $userGroup)
-    {
-
-        return $user->hasPermission(UserGroup::getModelPermission('modify'));
-
-    }
-
-    /**
-     * Determine whether the user can delete the user group.
-     *
-     * Groups can only be deleted if they are empty.
-     *
-     * @param  User         $user
-     * @param  UserGroup    $userGroup
-     *
-     * @return bool
-     */
-    public function delete(User $user, UserGroup $userGroup)
-    {
-
-        return ( $user->hasPermission(UserGroup::getModelPermission('delete')) && $userGroup->users->isEmpty() );
-
-    }
 }
