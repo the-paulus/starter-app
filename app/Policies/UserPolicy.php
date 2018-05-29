36d8bd02e3281cddc3c2064b195973038556c678
Paul Lyon <pmlyon@gmail.com>
2018-05-29 18:47:18 -0500

    Updated UserPolicy.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,7 +3,6 @@
 namespace App\Policies;
 
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 /**
  * Class UserPolicy defines policies specifically relating to performing actions on User models.
@@ -12,58 +11,9 @@ use Illuminate\Auth\Access\HandlesAuthorization;
  *
  * @package App\Policies
  */
-class UserPolicy
+class UserPolicy extends ApplicationPolicy
 {
-    use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  User   $user     User object to perform check on.
-     * @param  Model  $model    The model they are performing the task on.
-     *
-     * @return mixed
-     */
-    public function view(User $user, User $model)
-    {
-        return $user->hasPermission('access');
-    }
+    static protected $model = User::class;
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     *
-     * @return bool
-     */
-    public function create(User $user)
-    {
-        return $user->hasPermission('create');
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  User   $user     User object to perform check on.
-     * @param  Model  $model    The model they are performing the task on.
-     *
-     * @return bool
-     */
-    public function update(User $user, User $model)
-    {
-        return $user->hasPermission('modify');
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  User   $user     User object to perform check on.
-     * @param  Model  $model    The model they are performing the task on.
-     *
-     * @return bool
-     */
-    public function delete(User $user, User $model)
-    {
-        return $user->hasPermission('delete');
-    }
 }
