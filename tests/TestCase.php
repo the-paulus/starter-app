3ec8044cd5ca690e88c3399cedc8f754fb4030e6
Paul Lyon <pmlyon@gmail.com>
2018-05-30 00:20:23 -0500

    Added performJWTActionAs method.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -69,4 +69,35 @@ abstract class TestCase extends BaseTestCase
 
     }
 
+    /**
+     * Performs the $method as $user to the $endpoint URI using tokens.
+     *
+     * @param Authenticatable     $user                 User to perform the action as.
+     * @param string              $method               HTTP verb: GET, HEAD, PATCH, POST, PUT, or DELETE.
+     * @param string              $endpoint             URI to perform the action on. (e.g., api/user)
+     * @param integer|null        $expectedStatus       Expected status code to assert.
+     * @param array               $data                 Array that will be passed as the $data parameter of the $method function.
+     * @param array               $additional_headers   Array of headers to pass to the $method function.
+     *
+     * @return \Illuminate\Foundation\Testing\TestResponse|null
+     */
+    public function performJWTActionAs(Authenticatable $user, string $method, string $endpoint, $expectedStatus = null, array $data = array(), $additional_headers = array()) {
+
+        $token = JWTAuth::fromUser($user);
+        $headers = ['HTTP_Authorization' => 'Bearer ' . $token];
+        $headers = array_merge($headers, $additional_headers);
+
+        $response = $this->call($method, $endpoint, $data, [], [], $headers, null);
+
+
+        if( !is_null($expectedStatus) && $expectedStatus ) {
+
+            $this->assertEquals($expectedStatus, $response->getStatusCode(), $response->json());
+        }
+
+
+        return $response;
+
+    }
+
 }
