16f1c15c1dd0ab2d1bee9eebfc4ea2854809127d
Paul Lyon <pmlyon@gmail.com>
2018-05-30 21:37:17 -0500

    Added delete method  to UserPolicy.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,7 +2,11 @@
 
 namespace App\Policies;
 
+use Illuminate\Http\Response;
 use App\Models\User;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\Eloquent\Model;
+
 
 /**
  * Class UserPolicy defines policies specifically relating to performing actions on User models.
@@ -15,5 +19,25 @@ class UserPolicy extends ApplicationPolicy
 {
 
     static protected $model = User::class;
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  User   $user     User object to perform check on.
+     * @param  Model  $model    The model they are performing the task on.
+     *
+     * @return bool
+     */
+    public function delete(User $user, Model $model)
+    {
+
+        if($user->id == $model->id) {
+
+            throw new HttpException(Response::HTTP_BAD_REQUEST, json_encode(['data' => [], 'errors' => 'Cannot delete yourself.']));
+
+        }
+
+        return parent::delete($user, $model);
+    }
 
 }
