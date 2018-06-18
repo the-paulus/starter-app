c8b5a898c7f873cec0f38f9353636113d6638ee6
Paul Lyon <pmlyon@gmail.com>
2018-06-18 00:13:50 -0500

    Updated helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -47,3 +47,43 @@ if( !function_exists('camel_case_conversion') ) {
     }
 
 }
+
+if( !function_exists('print_c') ) {
+    function print_c(Closure $closure) {
+        $fncText = 'function (';
+        $rf = new ReflectionFunction($closure);
+        $fncParams = $rf->getParameters();
+
+        foreach($fncParams as $param) {
+
+            if($param->isArray()) {
+                $fncText .= 'array ';
+            } else if($param->isCallable()) {
+                $fncText .= 'Callable ';
+            } else if($param->isPassedByReference()) {
+                $fncText .= '&';
+            } else if($param->getClass()) {
+                $fncText .= $param->getClass();
+            }
+
+            $fncText .= $param->getName();
+
+            if($param->isOptional()) {
+                $fncText .= ' = ' . var_export($param->getDefaultValue(), TRUE);
+            }
+
+            $fncText .= ', ';
+        }
+
+        $fncText = str_replace_last(',', ')' . PHP_EOL, $fncText);
+
+        $fileCode = file($rf->getFileName());
+
+        for($ln = $rf->getStartLine(); $ln < $rf->getEndLine(); $ln++) {
+            $fncText .= $fileCode[$ln];
+        }
+
+        return $rf->getFileName() . PHP_EOL . $fncText;
+
+    }
+}
