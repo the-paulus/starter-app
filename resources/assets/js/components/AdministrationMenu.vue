de3f9b7c47068dc92db7d52edb2a7cc86092ad16
Paul Lyon <pmlyon@gmail.com>
2018-07-24 15:24:57 -0500

    Added AdministrationMenu Vue component.

---
+++ resources/assets/js/components/AdministrationMenu.vue
@@ -0,0 +1,40 @@
+<template>
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>Administration</a>
+        <ul class="dropdown-menu">
+            <router-link to="/groups" tag="li"><a>Groups</a></router-link>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        mounted: function () {
+
+        },
+        data: function () {
+            return {
+
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            hasGroupPermissions: function () {
+                console.log(document.cookie)
+                return true
+            },
+            hasSettingPermissions: function () {
+                return true
+            },
+            hasUserPermissions: function () {
+                return true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
