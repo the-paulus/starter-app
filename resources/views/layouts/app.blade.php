729175508a7f21d31d3cbd8f7acd060b274e6ba7
Paul Lyon <pmlyon@gmail.com>
2018-07-24 16:13:02 -0500

    Updated app blade template.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="#">Feedback</a></li>
+                        <administration-menu></administration-menu>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -48,7 +49,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
