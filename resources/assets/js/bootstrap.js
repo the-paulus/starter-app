39150cec39f1bd52c79363777b38b089c8562ab9
Paul Lyon <pmlyon@gmail.com>
2018-07-24 16:14:52 -0500

    Added validateStatus function to ensure all responses are handled by the application.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -22,7 +22,9 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.defaults.validateStatus = function (status) {
+    return true
+}
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
