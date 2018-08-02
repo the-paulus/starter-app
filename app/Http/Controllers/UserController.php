a16b8aad46a921ece0e1161c6c95950f1ee082da
Paul Lyon <pmlyon@gmail.com>
2018-08-02 15:15:12 -0500

    Added default sort and order to UserController.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,9 +17,14 @@ class UserController extends Controller
      */
     protected static $model = User::class;
 
+    /**
+     * @var string $default_sort Name of the field to sort on by default.
+     */
+    protected static $default_sort = 'last_name';
 
-
-
-
+    /**
+     * @var string Default sorting order. Either ASC or DESC.
+     */
+    protected static $default_order = 'ASC';
 
 }
