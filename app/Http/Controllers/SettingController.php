80632cded8816f871709140e302e5dad2a00e9e5
Paul Lyon <pmlyon@gmail.com>
2018-08-02 15:17:36 -0500

    Added default sort and order to SettingController.

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -7,4 +7,14 @@ use App\Models\Setting;
 class SettingController extends Controller
 {
     protected static $model = Setting::class;
+
+    /**
+     * @var string $default_sort Name of the field to sort on by default.
+     */
+    protected static $default_sort = 'name';
+
+    /**
+     * @var string Default sorting order. Either ASC or DESC.
+     */
+    protected static $default_order = 'ASC';
 }
