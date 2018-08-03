4ec4fb4e4be040ff24801149820f8633aa37beb6
Paul Lyon <pmlyon@gmail.com>
2018-08-03 12:23:31 -0500

    Added default per_page variable.

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,7 +6,15 @@ use App\Models\Setting;
 
 class SettingController extends Controller
 {
-    protected static $model = Setting::class;
+    /**
+     * @var string Default sorting order. Either ASC or DESC.
+     */
+    protected static $default_order = 'ASC';
+
+    /**
+     * @var int Default number of items to list per page.
+     */
+    protected static $default_per_page = 15;
 
     /**
      * @var string $default_sort Name of the field to sort on by default.
@@ -14,7 +22,7 @@ class SettingController extends Controller
     protected static $default_sort = 'name';
 
     /**
-     * @var string Default sorting order. Either ASC or DESC.
+     * @var string $model Model class that the controller is bound to.
      */
-    protected static $default_order = 'ASC';
+    protected static $model = Setting::class;
 }
