ec3ee187868a7dfe335046c03f39efa8bff8f202
Paul Lyon <pmlyon@gmail.com>
2018-08-03 12:23:19 -0500

    Added default per_page variable.

--- app/Http/Controllers/SettingGroupController.php
+++ app/Http/Controllers/SettingGroupController.php
@@ -6,15 +6,23 @@ use App\Models\SettingGroup;
 
 class SettingGroupController extends Controller
 {
-    protected static $model = SettingGroup::class;
-    
+    /**
+     * @var string Default sorting order. Either ASC or DESC.
+     */
+    protected static $default_order = 'ASC';
+
+    /**
+     * @var int Default number of items to list per page.
+     */
+    protected static $default_per_page = 15;
+
     /**
      * @var string $default_sort Name of the field to sort on by default.
      */
     protected static $default_sort = 'name';
 
     /**
-     * @var string Default sorting order. Either ASC or DESC.
+     * @var string $model Model class that the controller is bound to.
      */
-    protected static $default_order = 'ASC';
+    protected static $model = SettingGroup::class;
 }
