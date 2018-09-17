5c64745ff5f5dad0c02bf4425eddfc9b9626a37d
Paul Lyon <pmlyon@gmail.com>
2018-09-17 14:19:00 -0500

    Updated LoginController.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
 {
@@ -20,12 +27,8 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+    private $user;
+    private $token;
 
     /**
      * Create a new controller instance.
@@ -35,5 +38,53 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    protected function authenticated(Request $request, User $user)
+    {
+
+        Log::info("User " . $user->email . " authenticated.");
+
+        return redirect(config('app.frontend_url') . '/home?token=' . JWTAuth::fromUser($user));
+
+    }
+
+    public function unauthorized()  {
+
+        throw new AccessDeniedHttpException('You do not have access to this application');
+
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Log::info("Invalidating token and logging out " . Auth::user()->email);
+
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
+
+    public function emulateUser($id) {
+
+        $this->user = User::findOrFail($id);
+
+        $this->authorize('emulate', $this->user);
+
+        $this->token = JWTAuth::customClaims(['emu' => true])->fromUser($this->user);
+
+        return response()->json(['token' => $this->token, 'forceTokenReload' => TRUE], Response::HTTP_OK)->header('Authorization', ('Bearer '.$this->token));
     }
 }
