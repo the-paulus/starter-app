16f41cafc2a7c492cee6eb4a1d5ff8b1da3b460c
Paul Lyon <pmlyon@gmail.com>
2018-10-05 14:24:52 -0500

    Disabled create, update, and delete operations for Permission model.

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -13,4 +13,47 @@ class PermissionPolicy extends ApplicationPolicy
 
     protected static $model = Permission::class;
 
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  User  $user  User object to perform check on.
+     *
+     * @return bool Will always return false because we don't want users creating permissions on the fly.
+     */
+    public function create(User $user)
+    {
+
+        return FALSE;
+
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  User   $user     User object to perform check on.
+     * @param  Model  $model    The model they are performing the task on.
+     *
+     * @return bool Will always return false because we don't want users updating permissions on the fly.
+     */
+    public function update(User $user, Model $model)
+    {
+
+        return FALSE;
+
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  User   $user     User object to perform check on.
+     * @param  Model  $model    The model they are performing the task on.
+     *
+     * @return bool Will always return false because we don't want users updating permissions on the fly.
+     */
+    public function delete(User $user, Model $model)
+    {
+
+        return FALSE;
+
+    }
 }
