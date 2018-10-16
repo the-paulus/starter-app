f84cce3224ac62254c57d6dd6fd8de4c3443e650
Paul Lyon <pmlyon@gmail.com>
2018-10-16 16:07:57 -0500

    Updated method documentation.

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use http\Env\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
+/**
+ * ForgotPasswordController class provides the logic to handle requests for user to reset their password.
+ *
+ * @package App\Http\Controllers\Auth
+ */
 class ForgotPasswordController extends Controller
 {
     /*
@@ -28,5 +35,21 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Send a reset link to the given user only if the user is authenticated by the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request) {
+
+        if( User::all()->firstWhere('email', '=', $request->input('email'))->auth_type == 'local' ) {
+
+            return SendsPasswordResetEmails::sendResetLinkEmail($request);
+
+        }
+
     }
 }
