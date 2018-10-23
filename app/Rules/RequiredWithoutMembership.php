6b8006b1fe2d3d273d11409415cddbc4943e93ca
Paul Lyon <pmlyon@gmail.com>
2018-10-22 23:34:51 -0500

    Added RequiredWitouthMembership rule.

---
+++ app/Rules/RequiredWithoutMembership.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class RequiredWithoutMembership implements Rule  {
+
+    use ValidatesAttributes;
+
+    /**
+     * @var array Array of group names.
+     */
+    private $groups = [];
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+
+        if( func_num_args() < 1 ) {
+
+            throw new \InvalidArgumentException();
+
+        } else if( func_num_args() == 1) {
+
+            if( !is_array(func_get_arg(1)) ) {
+
+                $this->groups[0] = func_get_arg(1);
+
+            } else {
+
+                $this->groups = func_get_arg(1);
+
+            }
+
+        } else {
+
+            $this->groups = func_get_args();
+
+        }
+
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value) {
+
+        $passes = TRUE;
+
+        foreach($this->groups as $group) {
+
+            if(Auth::user()->isMemberOf($group)) {
+
+                $passes = FALSE;
+
+            }
+
+        }
+
+        return $passes;
+
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message() {
+
+        return ':attribute is required.';
+
+    }
+}
