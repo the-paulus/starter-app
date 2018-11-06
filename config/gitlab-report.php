515ba6bdda6fec6098be3a1ed9780fb1a11b679a
Paul Lyon <pmlyon@gmail.com>
2018-11-05 18:03:01 -0600

    Updated gitlab-report config file.

--- config/gitlab-report.php
+++ config/gitlab-report.php
@@ -6,7 +6,12 @@ return [
     'project_id' => env('GITLAB_REPORT_PROJECT_ID'),
     'labels' => env('GITLAB_REPORT_LABELS',''),
     'ignore-exceptions' => [
-        \Symfony\Component\Console\Exception\CommandNotFoundException::class
+        \Symfony\Component\Console\Exception\CommandNotFoundException::class,
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Validation\ValidationException::class,
     ],
     'redacted-fields' => [
         'password',
