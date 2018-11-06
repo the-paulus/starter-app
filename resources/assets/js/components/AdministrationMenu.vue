0723e60611148260e5c4abd07c2589aecefede53
Paul Lyon <pmlyon@gmail.com>
2018-11-05 18:25:31 -0600

    Updated AdministrationMenu component.

--- resources/assets/js/components/AdministrationMenu.vue
+++ resources/assets/js/components/AdministrationMenu.vue
@@ -2,38 +2,35 @@
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>Administration</a>
         <ul class="dropdown-menu">
-            <router-link to="/settings" tag="li"><a>Settings</a></router-link>
-            <router-link to="/users" tag="li"><a>User Management</a></router-link>
+            <router-link v-if="hasSettingPermissions()" to="/settings" tag="li"><a>Settings</a></router-link>
+            <router-link v-if="hasUserPermissions()" to="/users" tag="li"><a>User Management</a></router-link>
         </ul>
     </li>
 </template>
 
 <script>
-    export default {
-        mounted: function () {
+export default {
+    mounted: function () {
 
-        },
-        data: function () {
-            return {
+    },
+    data: function () {
+        return {
 
-            }
-        },
-        computed: {
+        }
+    },
+    computed: {
 
+    },
+    methods: {
+        hasSettingPermissions: function () {
+            return true
         },
-        methods: {
-            hasGroupPermissions: function () {
-                console.log(document.cookie)
-                return true
-            },
-            hasSettingPermissions: function () {
-                return true
-            },
-            hasUserPermissions: function () {
-                return true
-            }
+        hasUserPermissions: function () {
+            console.log(this.$store.state.current_user)
+            return true
         }
     }
+}
 </script>
 
 <style scoped>
