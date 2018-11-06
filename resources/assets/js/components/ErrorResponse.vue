f7c2fc70a783d614ea20cd256e5415f51954b9d5
Paul Lyon <pmlyon@gmail.com>
2018-11-05 18:26:00 -0600

    Added ErrorResponse vuejs component.

---
+++ resources/assets/js/components/ErrorResponse.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="modal-header">
+            <h3 class="modal-title">An Error has Occurred</h3>
+        </div>
+        <div class="modal-body">
+            {{ message }}
+            <div class="row form-group">
+                <div class="col-lg-12">
+                    <label>
+                        If you would like to provide feed back about this error, please describe in detail what you were doing when the error arose.
+                        <textarea v-model="feedback"></textarea>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-6 text-right">
+                    <div class="btn-group">
+                        <button @click="$emit('close')" type="button" class="btn btn-default">
+                            <i class="fa fa-fw fa-undo"></i>Go Back
+                        </button>
+                        <button @click="sendFeedback()" type="button" class="btn btn-default">
+                            <i class="fa fa-fw" :class="{ 'fa-refresh fa-spin': isSending, 'fa-envelope': !isSending }"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ErrorResponse',
+    props: {
+        message: {
+          type: String
+        },
+        response: {
+            type: Object,
+        }
+    },
+    data: function () {
+        return {
+            isSending: false,
+            feedback: ''
+        }
+    },
+    methods: {
+        sendFeedback: function () {
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
