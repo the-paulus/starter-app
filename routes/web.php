623045eee67f9c0153c03c3292fe5b23c49ca29f
Paul Lyon <pmlyon@gmail.com>
2018-11-05 18:17:34 -0600

    Updated web.php routes.

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('authenticated', 'LoginController@authenticated');
-Route::get('unauthorized', 'LoginController@unauthorized');
+Route::get('authenticated', 'Auth\\LoginController@authenticated');
+Route::get('unauthorized', 'Auth\\LoginController@unauthorized');
 
 Route::middleware('auth')->get('token', function() {
-  return response()->json(['data' => ['token' => \Tymon\JWTAuth\Facades\JWTAuth::fromUser(\Illuminate\Support\Facades\Auth::user())]], 200);
+  return response()->json(['data' => ['token' => \Tymon\JWTAuth\Facades\JWTAuth::fromUser(\Illuminate\Support\Facades\Auth::user()), 'user' => \Illuminate\Support\Facades\Auth::user()]], 200);
+});
+
+Route::middleware('jwt.refresh')->get('refresh', function() {
+
+   return response()->json(['data' => [ 'message' => 'Token refreshed'] ])
+       ->header('Access-Control-Expose-Headers', 'Authorization');
+
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
