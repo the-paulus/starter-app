629673f6a153eaa1a0629f6ccf5008d63f378e4e
Paul Lyon <pmlyon@gmail.com>
2018-11-07 17:56:23 -0600

    Updated AdministrationMenu

--- resources/assets/js/components/AdministrationMenu.vue
+++ resources/assets/js/components/AdministrationMenu.vue
@@ -1,9 +1,9 @@
 <template>
-    <li class="dropdown">
+    <li v-if="showAdminMenu()" class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>Administration</a>
         <ul class="dropdown-menu">
-            <router-link v-if="hasSettingPermissions()" to="/settings" tag="li"><a>Settings</a></router-link>
-            <router-link v-if="hasUserPermissions()" to="/users" tag="li"><a>User Management</a></router-link>
+            <router-link v-if="hasPermission('update settings')" to="/settings" tag="li"><a>Settings</a></router-link>
+            <router-link v-if="hasPermission('update users')" to="/users" tag="li"><a>User Management</a></router-link>
         </ul>
     </li>
 </template>
@@ -19,15 +19,22 @@ export default {
         }
     },
     computed: {
-
+        current_user: function() {
+            return this.$store.state.current_user
+        }
     },
     methods: {
-        hasSettingPermissions: function () {
-            return true
+        hasPermission: function (permission) {
+            try {
+                return Object.values(this.current_user.user_permissions).includes(permission)
+            } catch(e) { }
         },
-        hasUserPermissions: function () {
-            console.log(this.$store.state.current_user)
-            return true
+        showAdminMenu: function() {
+            try {
+                var current_permissions = Object.values(this.current_user.user_permissions)
+                return current_permissions.includes('update settings') || current_permissions.includes('update users') || current_permissions.includes('update user groups')
+            } catch (e) { }
+
         }
     }
 }
