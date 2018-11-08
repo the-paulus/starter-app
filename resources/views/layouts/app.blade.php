f86f7c3106c688fc97cdec43b46883a8ad4e556a
Paul Lyon <pmlyon@gmail.com>
2018-11-07 18:41:56 -0600

    Updated application's main blade template file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="#">Feedback</a></li>
                         <administration-menu></administration-menu>
+                        <li><a href="#">Feedback</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -47,24 +47,13 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->first_name }} <span class="caret"></span>
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                    <span>Logout <i class="fa fa-fw fa-sign-out"></i></span>
                                 </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </li>
                         @endguest
                     </ul>
